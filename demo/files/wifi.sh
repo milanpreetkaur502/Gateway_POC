620e74569b49269d188cc6e7cb0d3b2495b02ab7
Pratyush Verma <2019eem1010@iitrpr.ac.in>
2021-10-14 16:34:35 +0530

    file update

--- demo/files/wifi.sh
+++ demo/files/wifi.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
+​
 SSID=$1
 PWD=$2
 security=$3
+​
 connmanctl scan wifi
 TEST=`connmanctl services | grep $SSID | awk '{print $1}'`
+​
 if [ $SSID == $TEST ];then
 HASH=`connmanctl services | grep $SSID | awk '{print $2}'`
 else
 HASH=`connmanctl services | grep $SSID | awk '{print $3}'`
 fi
 echo "found $SSID with hash $HASH"
-if [[ "$security" == "psk" ]];then
-    "cat << EOF > /var/lib/connman/$SSID-$security.config
-    [service_$HASH]
+​
+​
+if [[ $security == "psk" ]];then
+    
+    echo "[service_$HASH]
     Type=wifi
-    name=$SSID
+    Name=$SSID
     Passphrase=$PWD
-    AutoConnect = True
-    EOF"
+    AutoConnect = True" > /var/lib/connman/$SSID-psk.config
+​
     connmanctl connect $HASH
-elif [[ "$security" == "none" ]];then
-    #connect to wifi directly
-    "cat << EOF > /var/lib/connman/$SSID-$security.config
-    [service_$HASH]
+​
+elif [[ $security == "none" ]];then
+    
+    echo "[service_$HASH]
     Type=wifi
-    name=$SSID
-    EOF"
+    Name=$SSID
+    AutoConnect = True" > /var/lib/connman/$SSID-none.config
     
     connmanctl connect $HASH
     
 fi
+​
 echo "done...!"
+​
+​
 # $security should contain eithr psk or none
