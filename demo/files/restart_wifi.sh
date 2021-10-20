2970825422a683d6ee17e0c264718ef7b5af3067
Pratyush Verma <2019eem1010@iitrpr.ac.in>
2021-10-20 13:45:58 +0530

    wifi script updated

--- demo/files/restart_wifi.sh
+++ demo/files/restart_wifi.sh
@@ -1,47 +1,31 @@
 #!/bin/bash
-​
 a=`jq '.network.SSID' /etc/gateway/config.conf`
 b=`jq '.network.PASSPHRASE' /etc/gateway/config.conf`
 c=`jq '.network.SECURITY' /etc/gateway/config.conf`
-
-
 wifissid=`echo $a | sed 's/^.//' | sed 's/.$//'`
 wifipassword=`echo $b | sed 's/^.//' | sed 's/.$//'`
 wifisecurity=`echo $c | sed 's/^.//' | sed 's/.$//'`
-​
 connmanctl scan wifi
 TEST=`connmanctl services | grep $wifissid | awk '{print $1}'`
-​
 if [[ $wifissid == $TEST ]];then
 HASH=`connmanctl services | grep $wifissid | awk '{print $2}'`
 else
 HASH=`connmanctl services | grep $wifissid | awk '{print $3}'`
 fi
 echo "found $wifissid with hash $HASH"
-​
-​
-if [[ $wifisecurity == "psk" ]];then
-    
+if [[ $wifisecurity == "psk" ]];then 
     echo "[service_$HASH]
     Type=wifi
     Name=$wifissid
     Passphrase=$wifipassword
     AutoConnect = True" > /var/lib/connman/$wifissid-psk.config
-​
     connmanctl connect $HASH
-​
 elif [[ $wifisecurity == "none" ]];then
-    
     echo "[service_$HASH]
     Type=wifi
     Name=$wifissid
     AutoConnect = True" > /var/lib/connman/$wifissid-none.config
-    
-    connmanctl connect $HASH
-    
+    connmanctl connect $HASH 
 fi
-​
 echo "done...!"
-​
-​
 # $security should contain eithr psk or none
